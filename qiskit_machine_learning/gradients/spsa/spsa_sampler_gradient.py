318a0c13bb575d577d60e4c8b2e89c3d3e51e731
M. Emre Sahin <40424147+OkuyanBoga@users.noreply.github.com>
2024-09-30 14:07:45 +0100

    Update qiskit_machine_learning/gradients/spsa/spsa_sampler_gradient.py
    Co-authored-by: Edoardo Altamura <38359901+edoaltamura@users.noreply.github.com>

--- qiskit_machine_learning/gradients/spsa/spsa_sampler_gradient.py
+++ qiskit_machine_learning/gradients/spsa/spsa_sampler_gradient.py
@@ -113,7 +113,7 @@ class SPSASamplerGradient(BaseSamplerGradient):
         elif isinstance(self._sampler, BaseSamplerV2):
             job = self._sampler.run(cirs_params)
         else:
-            raise AlgorithmError("Wrong Sampler Type.") 
+            raise AlgorithmError(f"The accepted estimators are BaseSamplerV1 (deprecated) and BaseSamplerV2; got {type(self._sampler)} instead.") 
         
         try:
             results = job.result()
